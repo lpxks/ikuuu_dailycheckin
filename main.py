558fdc1fc6299c7fbfd0980c2e756ec821d6d713
lzx <93460847+lpxks@users.noreply.github.com>
2025-01-02 12:59:48 +0800

    test multiple accounts

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ session = requests.session()
 # 从设置的环境变量中的Variables多个邮箱和密码 ,分割
 emails = os.environ.get('EMAIL', '').split(',')
 passwords = os.environ.get('PASSWD', '').split(',')
-
+print(emails,passwords)
 # server酱
 SCKEY = os.environ.get('SCKEY')
 # PUSHPLUS
